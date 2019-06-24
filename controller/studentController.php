15c4acc7b7284d197f63ead1925aadc936fb5897
mikhawa <michaeljpitz@gmail.com>
2019-06-24 09:40:25 +0200

    instanciate thestudent with

--- controller/studentController.php
+++ controller/studentController.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
  *
  * Contrôleur gérant les étudiants
@@ -9,30 +10,42 @@
  */
 
 
-if(isset($_GET['addstudent'])){
+if (isset($_GET['addstudent'])) {
+    
     /*
      * on veut ajouter un stagiaire
      */
+
+    // si le formulaire n'est pas envoyé
+    if (empty($_POST)) {
+
+        // on utilise la méthode qui prend titre et id de toutes les sections
+        $recupSections = $thesectionM->creerMenu();
+
+        // appel de la vue avec le passage des sections
+        echo $twig->render("admin/student/ajoutAdminStudent.html.twig", ["sections" => $recupSections]);
+    }else{
+        
+        // le formulaire est envoyé
+            
+        // on va instancier la classe thestudent pour hydrater thename et thesurname
+        $student = new thestudent($_POST);
+        
+        var_dump($student);
+        
+    }
     
-    // on utilise la méthode qui prend titre et id de toutes les sections
-    $recupSections = $thesectionM->creerMenu();
-    
-    // appel de la vue avec le passage des sections
-    echo $twig->render("admin/student/ajoutAdminStudent.html.twig",["sections"=>$recupSections]);
-    
-}elseif(isset($_GET['update'])&& ctype_digit($_GET['update'])){
-    $idstagiaire= (int) $_GET['update'];
+} elseif (isset($_GET['update']) && ctype_digit($_GET['update'])) {
+    $idstagiaire = (int) $_GET['update'];
     /*
      * on veut modifier un stagiaire
      */
-
-}elseif(isset($_GET['delete'])&& ctype_digit($_GET['delete'])){
+} elseif (isset($_GET['delete']) && ctype_digit($_GET['delete'])) {
     /*
      * on veut supprimer un stagiaire
      */
-    $idstagiaire= (int) $_GET['delete'];
-    
-}else {
+    $idstagiaire = (int) $_GET['delete'];
+} else {
 
     /*
      * Page d'accueil
@@ -40,9 +53,8 @@ if(isset($_GET['addstudent'])){
 
 // récupérer tous les stagiaires avec les sections dans lesquelles ils sont, affichez les stagiaires qui n'ont pas de section également
     $recupStudents = $thestudentM->selectionnerAllStudent();
-    
+
 
     // appel de la vue avec le passage des étudiants
-    echo $twig->render("admin/student/accueilAdminStudent.html.twig",["student"=>$recupStudents]);
-
+    echo $twig->render("admin/student/accueilAdminStudent.html.twig", ["student" => $recupStudents]);
 }
